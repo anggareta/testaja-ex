a9e2db5196c59bf612047d5aec6ab4c9635a3fb4
anggarete <anggareta@hotmail.com>
2020-06-05 21:29:00 +0700

    revisi

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const http = require('http');
-
-//const hostname = '127.0.0.1';
+const express = require("express");
 const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  //res.end('Hello World');
-  res.end('[{"id":9,"name":"Glenna Reichert","username":"Delphine","email":"Chaim_McDermott@dana.io","address":{"street":"Dayna Park","suite":"Suite 449","city":"Bartholomebury","zipcode":"76495-3109","geo":{"lat":"24.6463","lng":"-168.8889"}},"phone":"(775)976-6794 x41206","website":"conrad.com","company":{"name":"Yost and Sons","catchPhrase":"Switchable contextually-based project","bs":"aggregate real-time technologies"}},'+
-  '{"id": 1,"name": "Leanne Graham","username": "Bret","email": "Sincere@april.biz","address": {"street": "Kulas Light","suite": "Apt. 556","city": "Gwenborough","zipcode": "92998-3874","geo": {"lng": "81.1496"}},"phone": "1-770-736-8031 x56442","website": "hildegard.org","company": {"name": "Romaguera-Crona","catchPhrase": "Multi-layered client-server neural-net","bs": "harness real-time e-markets"}}'+
-  ']');
-}).listen(port, () => {
-  console.log(`Listening on ${port}`)
+var app = express();
+
+app.get('/', function (req, res) {
+  // initialize the container for our data
+  const { headers, method, url } = req;
+  let body = [];
+  const responseBody = { headers, method, url, body };
+  //res.send(JSON.stringify(responseBody));
+  res.json(responseBody);
+
+  //res.send("Hello World!");
 });
 
-//server.listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`);
-//});
+
+app.get('/users', function (req, res) {
+  // initialize the container for our data
+  //const { headers, method, url } = req;
+  //let body = [];
+  //const responseBody = { headers, method, url, body };
+  //res.send(JSON.stringify(responseBody));
+  res.json([{"id":9,"name":"Glenna Reichert","username":"Delphine","email":"Chaim_McDermott@dana.io","address":{"street":"Dayna Park","suite":"Suite 449","city":"Bartholomebury","zipcode":"76495-3109","geo":{"lat":"24.6463","lng":"-168.8889"}},"phone":"(775)976-6794 x41206","website":"conrad.com","company":{"name":"Yost and Sons","catchPhrase":"Switchable contextually-based project","bs":"aggregate real-time technologies"}},
+	{"id": 1,"name": "Leanne Graham","username": "Bret","email": "Sincere@april.biz","address": {"street": "Kulas Light","suite": "Apt. 556","city": "Gwenborough","zipcode": "92998-3874","geo": {"lng": "81.1496"}},"phone": "1-770-736-8031 x56442","website": "hildegard.org","company": {"name": "Romaguera-Crona","catchPhrase": "Multi-layered client-server neural-net","bs": "harness real-time e-markets"}}]);
+
+  //res.send("Hello World!");
+});
+
+var server = app.listen(port, function () {
+  console.log('Server is running..');
+});
