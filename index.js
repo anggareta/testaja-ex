3a7c757a252292e5132f4cbf6bb2cc4852bfc4b0
anggarete <anggareta@hotmail.com>
2020-06-08 12:29:36 +0700

    kembali

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express')
-//const path = require('path')
-const PORT = process.env.PORT || 3000
+const express = require("express");
+const PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -17,7 +16,29 @@ app.get('/', function (req, res) {
 
 app.get('/users', function (req, res) {
 
-  res.json([
+  res.json([{"id":9,"name":"Glenna Reichert","username":"Delphine","email":"Chaim_McDermott@dana.io","address":{"street":"Dayna Park","suite":"Suite 449","city":"Bartholomebury","zipcode":"76495-3109","geo":{"lat":"24.6463","lng":"-168.8889"}},"phone":"(775)976-6794 x41206","website":"conrad.com","company":{"name":"Yost and Sons","catchPhrase":"Switchable contextually-based project","bs":"aggregate real-time technologies"}},
+	{"id": 1,"name": "Leanne Graham","username": "Bret","email": "Sincere@april.biz","address": {"street": "Kulas Light","suite": "Apt. 556","city": "Gwenborough","zipcode": "92998-3874","geo": {"lng": "81.1496"}},"phone": "1-770-736-8031 x56442","website": "hildegard.org","company": {"name": "Romaguera-Crona","catchPhrase": "Multi-layered client-server neural-net","bs": "harness real-time e-markets"}}]);
+
+});
+
+var server = app.listen(PORT, function () {
+  console.log('Server is running..');
+});
+
+
+/* batasnya disini */
+
+/*
+const http = require('http');
+
+//const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  //res.end('Hello World');
+  res.end(`[
   {
     "id": 1,
     "name": "Leanne Graham",
@@ -248,11 +269,8 @@ app.get('/users', function (req, res) {
       "bs": "target end-to-end models"
     }
   }
-]);
-
+]`);
+}).listen(port, () => {
+  console.log(`Listening on ${port}`)
 });
-
-var server = app.listen(PORT, function () {
-  console.log(`Listening on ${ PORT }`);
-});
-
+*/
