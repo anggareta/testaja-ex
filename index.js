3f03549a344ecb027f17d55b90f98eb7cdfb8fec
anggarete <anggareta@hotmail.com>
2020-06-08 11:39:31 +0700

    pusing

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.get('/', function (req, res) {
+  // initialize the container for our data
+  //const { headers, method, url } = req;
+  //let body = [];
+  //const responseBody = { headers, method, url, body };
+  //res.send(JSON.stringify(responseBody));
+  //res.json(responseBody);
+
+  res.send("<h1>Hello World!</h1>");
+});
+
+var server = app.listen(PORT, function () {
+  console.log(`Listening on ${ PORT }`);
+});
+
